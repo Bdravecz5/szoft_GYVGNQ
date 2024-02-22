355118e5b1646663d84048abc5d30a1eaa1c46fa
Barnabás Dravecz <barnabas.dravecz@stud.uni-corvinus.hu>
2024-02-22 10:42:21 +0100

    Pascal projekt

--- szoft_GYVGNQ/Form1.cs
+++ szoft_GYVGNQ/Form1.cs
@@ -6,5 +6,23 @@ namespace szoft_GYVGNQ
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    Button b = new Button();
+                    b.Text = (i * j).ToString();
+                    b.Left = 100 * i;
+                    b.Top = 100 * j;
+
+                    Controls.Add(b);
+
+                }
+            }
+
+        }
     }
 }
